19fc479bde45705bdf3e38fdbbd418f698c6db98
Pushkarsuthar9521 <pushkar.suthar9521@gmail.com>
2023-05-22 22:35:51 +0530

    majority element

--- dsa/majorityelement.cpp
+++ dsa/majorityelement.cpp
@@ -18,3 +18,15 @@ int majorityElement(int arr[], int n)
     }
     return -1;
 }
+
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for (int i = 0; i<n;i++)
+    {
+        cin >> arr[i];
+    }
+    cout << majorityElement(arr, n);
+}
