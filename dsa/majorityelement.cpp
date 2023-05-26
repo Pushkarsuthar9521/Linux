74b9a85a2972408bf0b6f4a3d055f972757a7675
Pushkarsuthar9521 <pushkar.suthar9521@gmail.com>
2023-05-26 20:06:40 +0530

    Moore voting algo

--- dsa/majorityelement.cpp
+++ dsa/majorityelement.cpp
@@ -1,19 +1,51 @@
 
-//....
+//Moore voting algo....
 // majority elements
+
+int mjele(int arr[], int size)
+{
+    int count = 0;
+    int el = 0;
+    for (int i = 0; i < size;i++)
+    {
+        if(count==0)
+            el = arr[i];
+        
+        if(el==arr[i])
+            count++;
+        else
+            count--;
+    }
+    count = 0;
+    for (int i = 0; i < size; i++)
+    {
+        if (el == arr[i])
+            count++;
+        }
+
+        if(count>size/2)
+        return el;
+        else
+        return -1;
+}
+
+
+
+
+//naive approach
 #include<bits/stdc++.h>
 using namespace std;
 
-int majorityElement(int arr[], int n)
+int majorityElement(int arr[], int size)
 {
     int count=0;
-    for(int i=0;i<n;i++)
+    for(int i=0;i<size;i++)
     {
-        for(int j=0; j<n;j++)
+        for(int j=0; j<size;j++)
         {
             if(arr[i]==arr[j])
                 count++;
-            if(count>n/2)
+            if(count>size/2)
                 return arr[i];
         }
         count=0;
@@ -23,12 +55,15 @@ int majorityElement(int arr[], int n)
 
 int main()
 {
-    int n;
-    cin>>n;
-    int arr[n];
-    for (int i = 0; i<n;i++)
+    int size;
+    cin>>size;
+    int arr[size];
+    for (int i = 0; i<size;i++)
     {
         cin >> arr[i];
     }
-    cout << majorityElement(arr, n);
+    cout << "Naive approach :";
+    cout << majorityElement(arr, size)<<endl;
+    cout << "Moore voting algo: ";
+    cout << mjele(arr, size);
 }
