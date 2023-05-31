e34240bfd431bef40e2de4394d915bd774e0a476
Pushkarsuthar9521 <pushkar.suthar9521@gmail.com>
2023-05-31 17:18:11 +0530

    count inversions

--- dsa/tempCodeRunnerFile.cpp
+++ dsa/tempCodeRunnerFile.cpp
@@ -1,35 +1,34 @@
-// move zeros
-
-
+// count inversion
 #include<bits/stdc++.h>
 using namespace std;
+
+
+//brute force method
+void bruteforce(int a[], int n)
+{
+    int count=0;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=i+1;j<n;j++)
+        {
+            if(a[i]>a[j])
+                count++;
+        }
+    }
+    cout<<count++;
+}
 
 int main()
 {
     int n;
     cin>>n;
     int a[n];
-    int loc;
-    
-    for(int i=0;i<n;i++) 
+    for(int i=0;i<n;i++)
     {
         cin>>a[i];
     }
 
-      loc=0;
-    for(int i=0;i<n;i++)
-    {
-        
-        if(a[i]!=0)
-         {
-            swap(a[loc],a[i]);
-            loc++;
-         } 
-    }
+    bruteforce(a,n);
 
-    for(int i=0;i<n;i++)
-    {
-        cout<<a[i]<<" ";
-    }
+    return 0;
 }
-
